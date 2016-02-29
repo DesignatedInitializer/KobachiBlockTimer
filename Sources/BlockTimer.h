14ff64d1fab1f2a55b4ed53badccc7c2de9b2b1e
Alexander I. Kovács <aik@kobachi.jp>
2016-02-29 16:40:31 +0900

    Add Carthage support
    - Share the scheme
    - Add public umbrella header

---
+++ Sources/BlockTimer.h
@@ -0,0 +1,9 @@
+//
+//  BlockTimer.h
+//  BlockTimer
+//
+//  Created by Alexander I. Kovács on 2/29/16.
+//  Copyright © 2016 Alexander I. Kovács. All rights reserved.
+//
+
+#import <BlockTimer/NSTimer+KobachiBlockTimer.h>
